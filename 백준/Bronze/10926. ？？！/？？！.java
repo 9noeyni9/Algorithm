fd652d655e9ee4bb2e237737e4c0f9dcb4c1fda4
9noeyni9 <111550787+9noeyni9@users.noreply.github.com>
2023-12-22 20:52:22 +0900

    [Bronze V] Title: ??!, Time: 216 ms, Memory: 17780 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+class Main {
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        
+        String id = scan.nextLine();
+        System.out.println(id + "??!");
+    }
+}
