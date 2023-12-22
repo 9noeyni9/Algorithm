3e2cfface76e269e730ce05db6cd92fc53384f5b
9noeyni9 <111550787+9noeyni9@users.noreply.github.com>
2023-12-22 20:25:04 +0900

    [Bronze V] Title: A-B, Time: 216 ms, Memory: 17736 KB -BaekjoonHub

---
+++ 백준/Bronze/1001. A－B/A－B.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+class Main{
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        int A,B =0;
+        A = scan.nextInt();
+        B = scan.nextInt();
+        System.out.println(A-B);
+    }
+}
