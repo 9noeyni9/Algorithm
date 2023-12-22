ada8ce2f946cb7f374cd18fe9c72a268f900c3d5
9noeyni9 <111550787+9noeyni9@users.noreply.github.com>
2023-12-22 20:39:56 +0900

    [Bronze V] Title: A/B, Time: 220 ms, Memory: 17988 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+class Main{
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        
+        double A, B =0;
+        A = scan.nextDouble();
+        B = scan.nextDouble();
+        System.out.println(A/B);
+    }
+}
