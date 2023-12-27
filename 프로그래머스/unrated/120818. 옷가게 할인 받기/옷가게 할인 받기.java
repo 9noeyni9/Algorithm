f7f48dc17268779e5fb40ecd3822ebac322270ce
9noeyni9 <111550787+9noeyni9@users.noreply.github.com>
2023-12-28 03:34:19 +0900

    [level unrated] Title: 옷가게 할인 받기, Time: 0.21 ms, Memory: 72.7 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120818. 옷가게 할인 받기/옷가게 할인 받기.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int solution(int price) {
+        double answer_double = 0;
+        int answer = 0;
+        if(price >= 500000)
+            answer_double = (double)price * 0.8;
+        else if(price >= 300000)
+            answer_double = (double)price * 0.9;
+        else if(price >= 100000)
+            answer_double = (double)price * 0.95;
+        else
+            answer_double = (double)price;
+        answer = (int)(Math.floor(answer_double));
+        return answer;
+    }
+}
