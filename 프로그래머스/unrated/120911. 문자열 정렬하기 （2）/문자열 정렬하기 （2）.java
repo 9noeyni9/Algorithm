4d679a146579d4c95dd84f900c34d876db12696a
9noeyni9 <111550787+9noeyni9@users.noreply.github.com>
2024-01-06 09:55:20 +0900

    [level unrated] Title: 문자열 정렬하기 (2), Time: 0.84 ms, Memory: 75.1 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120911. 문자열 정렬하기 （2）/문자열 정렬하기 （2）.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String my_string) {
+        String[] arr= (my_string.toLowerCase()).split("");
+        Arrays.sort(arr);
+        return String.join("",arr);
+    }
+}
