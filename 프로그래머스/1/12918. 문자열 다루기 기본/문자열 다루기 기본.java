8f96186c20881e14902043fa4906bd3dd05a20ca
9noeyni9 <111550787+9noeyni9@users.noreply.github.com>
2024-01-18 09:51:30 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.01 ms, Memory: 73.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean solution(String s) {
+        boolean answer = true;
+        int n = 0;
+        int count = 0;
+        if(s.length() == 4 || s.length() == 6){
+            for(int i = 0 ; i < s.length();i++){
+            n = s.charAt(i);
+            if(n >='0' && n <= '9'){
+                count++;
+            }
+        }
+        answer = count == s.length() ? true : false;   
+        }else{
+            answer = false;
+        }
+        return answer;
+    }
+}
