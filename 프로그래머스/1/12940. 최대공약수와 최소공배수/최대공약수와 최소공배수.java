86f91aceaf42a1550acd72454df4ab88c23a605c
9noeyni9 <111550787+9noeyni9@users.noreply.github.com>
2024-03-08 09:14:36 +0900

    [level 1] Title: 최대공약수와 최소공배수, Time: 0.04 ms, Memory: 76.9 MB -BaekjoonHub

--- 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.java
+++ 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.java
@@ -1,14 +1,14 @@
 class Solution {
     public int[] solution(int n, int m) {
         int[] answer = new int[2];
-        int gcd_result = gcd(Math.max(m,n),Math.min(m,n));
-        answer[0] = gcd_result;
-        answer[1] = m*n/gcd_result;
+        int gcdResult = gcd(Math.max(m,n),Math.min(m,n));
+        answer[0] = gcdResult;
+        answer[1] = m*n/gcdResult;
         return answer;
     }
     
     public int gcd(int m, int n){
         if(m%n == 0) return n;
-        return gcd(n,m%n);
+        return gcd(n, m%n);
     }
 }
