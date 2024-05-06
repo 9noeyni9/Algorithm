e1423045ea5e9965628f109bc3f46bdee1cfd984
9noeyni9 <111550787+9noeyni9@users.noreply.github.com>
2024-05-07 02:06:48 +0900

    [level 0] Title: 수 조작하기 1, Time: 4.82 ms, Memory: 87.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/181926. 수 조작하기 1/수 조작하기 1.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int solution(int n, String control) {
+        for(int i = 0; i < control.length();i++){
+            switch(control.substring(i,i+1)){
+                case "w" :
+                    n += 1;
+                    break;
+                case "s" :
+                    n -= 1;
+                    break;
+                case "d" : 
+                    n += 10;
+                    break;
+                case "a" :
+                    n -= 10;
+                    break;
+            }
+        }
+        return n;
+    }
+}
