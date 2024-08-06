de77cc082bf43f9596d8ba25097c741751bd6ec4
9noeyni9 <111550787+9noeyni9@users.noreply.github.com>
2024-08-07 01:48:52 +0900

    [Bronze III] Title: 오븐 시계, Time: 148 ms, Memory: 16048 KB -BaekjoonHub

--- 백준/Bronze/2525. 오븐 시계/오븐 시계.java
+++ 백준/Bronze/2525. 오븐 시계/오븐 시계.java
@@ -1,17 +1,33 @@
-import java.util.*;
+import java.io.*;
 
-class Main{
-    public static void main(String[] args){        
-        Scanner scan = new Scanner(System.in);
-        int h = scan.nextInt();
-        int m = scan.nextInt();
-        int ovenTime = scan.nextInt();
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
         
-        int result = h*60 + m + ovenTime;
+        String[] numbers = reader.readLine().split(" ");
+        int hour = Integer.parseInt(numbers[0]);
+        int minute = Integer.parseInt(numbers[1]);
         
-        int hour = (result /60) %24;
-        int minute = result % 60;
+        int cookTime = Integer.parseInt(reader.readLine());
         
-        System.out.println(hour + " " +minute);
+        int cookHour = (cookTime / 60)%24;
+        int cookMinute = cookTime % 60;
+        
+        int afterHour = 0;
+        int afterMinute = 0;
+               
+        if(minute + cookMinute >= 60){
+            cookHour++;
+            afterMinute = (minute + cookMinute) % 60;
+        } else {
+            afterMinute = minute + cookMinute;
+        }
+        
+        afterHour = (hour + cookHour) % 24;
+        
+        writer.write(afterHour + " " + afterMinute + "\n");
+        writer.flush();
+        writer.close();
     }
 }
