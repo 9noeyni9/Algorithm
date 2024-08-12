525a8a0edd6139bf62f5e547427d8fc7be9f659b
9noeyni9 <111550787+9noeyni9@users.noreply.github.com>
2024-08-12 22:59:11 +0900

    [Bronze V] Title: 아스키 코드, Time: 200 ms, Memory: 17572 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        
+        char chr = scanner.next().charAt(0);
+        
+        System.out.println((int)chr);
+    }
+}
