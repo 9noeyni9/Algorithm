fa74b4ba773ec73c265951c04542f5fb2fc9da08
9noeyni9 <111550787+9noeyni9@users.noreply.github.com>
2024-08-28 15:16:43 +0900

    [Silver IV] Title: 듣보잡, Time: 312 ms, Memory: 29452 KB -BaekjoonHub

---
+++ 백준/Silver/1764. 듣보잡/듣보잡.java
@@ -0,0 +1,53 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        StringBuilder sb = new StringBuilder();
+        
+        // 중복값 판단 위해 set 사용
+        HashSet<String> set = new HashSet<>();
+        
+        String[] array = br.readLine().split(" ");
+        int N = Integer.parseInt(array[0]);
+        int M = Integer.parseInt(array[1]);
+        
+        for(int i = 0; i < N+M ; i++){
+            String str = br.readLine();
+            
+            // set contains 메서드 이용해서 존재하면 sb 문자열에 추가 후에 공백도 추가
+            if(set.contains(str)){
+                sb.append(str).append(" ");
+            }
+            set.add(str);
+        }
+        
+        if(set.size() == (N+M)){
+            bw.write(0 + "\n");
+            bw.flush();
+            bw.close();
+            return;
+        }
+            
+        
+        // 문자열 앞 뒤 공백 제거 후 배열 생성
+        String[] solutions = sb.toString().trim().split(" ");
+        
+        // 사전 순으로 정렬
+        Arrays.sort(solutions);
+        
+        // 배열 크기 출력
+        bw.write(solutions.length + "\n");
+        
+        // 사전 순으로 출력
+        for(int j = 0; j < solutions.length;j++){
+            bw.write(solutions[j] + "\n");
+        }
+        
+        bw.flush();
+        bw.close();
+    }
+}
