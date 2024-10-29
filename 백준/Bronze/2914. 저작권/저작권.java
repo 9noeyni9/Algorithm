79d3efc7ed743dd14dfa672aa36e51160b46991b
9noeyni9 <111550787+9noeyni9@users.noreply.github.com>
2024-10-29 23:37:51 +0900

    [Bronze III] Title: 저작권, Time: 124 ms, Memory: 15720 KB -BaekjoonHub

---
+++ 백준/Bronze/2914. 저작권/저작권.java
@@ -0,0 +1,19 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        String[] array = br.readLine().split(" ");
+        
+        int n = Integer.parseInt(array[0]);
+        int m = Integer.parseInt(array[1]);
+        
+        br.close();
+        
+        bw.write(n * (m-1) +1 + "\n");
+        bw.flush();
+        bw.close();
+    }
+}
