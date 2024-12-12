ddf8d8603940fe77e8e4b43872f46fa9bcff335b
9noeyni9 <111550787+9noeyni9@users.noreply.github.com>
2024-12-13 02:44:49 +0900

    [level 0] Title: 왼쪽 오른쪽, Time: 0.04 ms, Memory: 88.5 MB -BaekjoonHub

--- 프로그래머스/0/181890. 왼쪽 오른쪽/왼쪽 오른쪽.java
+++ 프로그래머스/0/181890. 왼쪽 오른쪽/왼쪽 오른쪽.java
@@ -4,25 +4,11 @@ class Solution {
     public String[] solution(String[] str_list) {
         String[] answer = {};
         
-        ArrayList<String> list = new ArrayList<>();
-        int count = 0;
-        
         for(int i = 0; i < str_list.length; i++) {
-            if(count == 0) {
-                if(str_list[i].equals("l")){
-                    count++;
-                    break;
-                } else if(str_list[i].equals("r")){
-                    count++;
-                    list = new ArrayList<String>();
-                    continue;
-                }
-            }
-            list.add(str_list[i]);            
-        }
-        
-        if(list.size() != str_list.length){
-            answer = list.toArray(new String[0]);
+            if(str_list[i].equals("l"))
+                return Arrays.copyOfRange(str_list,0,i);
+            else if(str_list[i].equals("r"))
+                return Arrays.copyOfRange(str_list,i+1,str_list.length);
         }
         return answer;
     }
