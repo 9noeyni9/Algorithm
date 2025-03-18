4ac9ae9ec59e0886698b012f7c386d3b9d85fffd
9noeyni9 <111550787+9noeyni9@users.noreply.github.com>
2025-03-18 17:13:34 +0900

    [level 0] Title: 수 조작하기 2, Time: 10.69 ms, Memory: 89 MB -BaekjoonHub

---
+++ 프로그래머스/0/181925. 수 조작하기 2/수 조작하기 2.java
@@ -0,0 +1,17 @@
+class Solution {
+    public String solution(int[] numLog) {
+        StringBuilder answer = new StringBuilder();
+        
+        for(int i = 0; i < numLog.length-1; i++){
+            int calc = numLog[i+1] - numLog[i];
+            switch(calc){
+                case 1: answer.append("w");break;
+                case -1: answer.append("s");break;
+                case 10: answer.append("d"); break;
+                case -10: answer.append("a"); break;
+            }
+        }
+        
+        return answer.toString();
+    }
+}
