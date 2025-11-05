f0467055113cee968501f174cfd697fffb18ef3f
9noeyni9 <111550787+9noeyni9@users.noreply.github.com>
2025-11-05 22:15:09 +0900

    [level 1] Title: K번째수, Time: 0.44 ms, Memory: 72.8 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.java
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -3,10 +3,15 @@ import java.util.*;
 class Solution {
     public int[] solution(int[] array, int[][] commands) {
         int[] answer = new int[commands.length];
-        for(int i =0;i < commands.length; i++){
-            int[] copyOfArray = Arrays.copyOfRange(array, commands[i][0]-1, commands[i][1]);
-            Arrays.sort(copyOfArray);
-            answer[i] = copyOfArray[commands[i][2]-1];
+        for(int i = 0; i < commands.length; i++) {
+            int[] arr = new int[commands[i][1] - commands[i][0]+1];
+            int count = 0;
+            for(int j = commands[i][0]-1; j < commands[i][1]; j++) {
+                arr[count] = array[j];
+                count++;
+            }
+            Arrays.sort(arr);
+            answer[i] = arr[commands[i][2]-1];
         }
         return answer;
     }
